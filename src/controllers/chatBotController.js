f6fe47e68fef64b09af2716c0b3647e02e7e7802
llemanhbin <llemanhbin@gmail.com>
2023-05-04 14:41:20 +0700

    up

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -8,8 +8,9 @@ function handleMessage(sender_psid, received_message) {
 
   // Checks if the message contains text
   if (received_message.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
+    response = {
+      text: `Bạn vừa nói là ${received_message.text}`,
+    };
     if (received_message.text == "Chào") {
       response = {
         text: `Chào bạn`,
