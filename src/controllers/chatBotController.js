a30fcc54b98a9a66c96c8cf86cc45ad1d9ae57f9
llemanhbin <llemanhbin@gmail.com>
2023-05-04 14:30:20 +0700

    upload test

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -10,9 +10,15 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    response = {
-      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
-    };
+    if (received_message.text == "Chào") {
+      response = {
+        text: `Chào bạn`,
+      };
+    } else if (received_message.text == "Hello") {
+      response = {
+        text: `Hello`,
+      };
+    }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
