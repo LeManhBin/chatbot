8f0f2663acc9b44afe7373b947e9546550d0f6ea
llemanhbin <llemanhbin@gmail.com>
2023-05-04 13:34:09 +0700

    upload image

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -8,18 +8,44 @@ function handleMessage(sender_psid, received_message) {
 
   // Checks if the message contains text
   if (received_message.text) {
-    // Creates the payload for a basic text message, which
+    // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      text: `Bạn đã gửi 1 tin nhắn: "${received_message.text}". Bây giờ gửi tôi 1 ảnh!`,
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
     };
   } else if (received_message.attachments) {
-    // Gets the URL of the message attachment
+    // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
-    console.log("attachment_url: " + attachment_url);
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Đây có phải ảnh của bạn không?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Có!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "Không!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
   }
 
-  // Sends the response message
+  // Send the response message
   callSendAPI(sender_psid, response);
 }
 
@@ -48,6 +74,22 @@ function callSendAPI(sender_psid, response) {
       }
     }
   );
+}
+
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
 const chatBotController = {
