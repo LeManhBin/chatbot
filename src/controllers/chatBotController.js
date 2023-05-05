ba84d638d151cc012438316eccbc3c473a17948b
llemanhbin <llemanhbin@gmail.com>
2023-05-05 15:00:54 +0700

    axios

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import axios from "axios";
 import request from "request";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
